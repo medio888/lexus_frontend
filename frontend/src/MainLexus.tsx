9dcf09ea4dae9e4f8b954e7c40ceee034b267a86
Meder Asanbekov <asanbekovmeder88@gmail.com>
2026-02-24 09:48:59 +0600

    main

---
+++ frontend/src/MainLexus.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const MainLexus: React.FC = () => {
+  return (
+    <main>
+      <section>
+        <img
+          src="https://scene7.toyota.eu/is/image/toyotaeurope/GX_layout_overtrail_luxury-ptif:Medium-Landscape?ts=1769986383102&resMode=sharp2&op_usm=1.75,0.3,2,0"
+          alt="Lexus GX"
+        />
+        <div>
+          <h1>Добро пожаловать</h1>
+
+          <div>
+            <a href="#">Новый Lexus LX</a>
+            <a href="#">Новый Lexus NX</a>
+            <a href="#">Брошюры и прайс-листы</a>
+            <a href="#">спецпредложения сервиса</a>
+          </div>
+
+          <div>
+            <a href="#">Абсолютно новый GX</a>
+          </div>
+
+          <div>
+            <a href="#">
+              <img
+                src="https://i.postimg.cc/bNNhRTkk/photo-2026-02-24-09-17-32.jpg"
+                alt="RX Hybrid"
+              />
+              Узнать о RX Hybrid
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <img
+            src="https://i.postimg.cc/RZYJmqvx/Snimok-ekrana-2026-02-24-093030.png"
+            alt="Lexus RX"
+          />
+          <h1>Абсолютно новый Lexus RX</h1>
+          <p>модели</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <img
+            src="https://i.postimg.cc/Y0Xqxy9r/Snimok-ekrana-2026-02-24-093432.png"
+            alt="Lexus LX"
+          />
+          <h1>Старт продаж нового Lexus LX</h1>
+          <p>модели</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>Lexus Relax</h1>
+          <img
+            src="https://i.postimg.cc/VLxccc2V/Snimok-ekrana-2026-02-24-093747.png"
+            alt="Lexus Relax"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default MainLexus;
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,15 +1,28 @@
-function Home({ categories }: { categories: { id: number; name: string }[] }) {
+import MainLexus from "../MainLexus";
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface HomeProps {
+  categories: Category[];
+}
+
+const Home: React.FC<HomeProps> = ({ categories }) => {
   return (
     <div style={{ padding: "20px" }}>
+      <MainLexus />
+
       <h1>Категории</h1>
 
-      {categories.map(item => (
+      {categories.map((item) => (
         <div key={item.id} style={{ marginBottom: "10px" }}>
           {item.name}
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default Home;
